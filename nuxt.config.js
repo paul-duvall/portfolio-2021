39da85bd60f22b1740a23a7c17088036f4f17afb
Paul Duvall <paul.duvall@myfuturechoice.com>
2021-04-27 18:08:27 +0100

    feat: add google fonts package

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/google-fonts',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
