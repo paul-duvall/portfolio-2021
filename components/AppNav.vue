761650643117c62a5a14d248cbdae376e5e569dc
Paul Duvall <paul.duvall@myfuturechoice.com>
2021-07-18 13:01:24 +0100

    feat: add burger menu

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -4,7 +4,38 @@
       pjd<span class="nav-logo-dot">.</span>
     </div>
     <nav class="nav-navigation--container">
-      <ul class="nav-navigation--list">
+      <button
+        class="nav-navigation--burger"
+        @click.prevent="toggleMenu"
+      >
+        x
+      </button>
+      <transition name="slide">
+        <div
+          v-if="menuIsVisible"
+          class="nav-navigation--slide-menu"
+        >
+          <li class="nav-navigation--slide-item">
+            <nuxt-link to="/" class="nav-navigation--slide-link">
+              Home
+            </nuxt-link>
+          </li>
+          <li class="nav-navigation--slide-item">
+            <nuxt-link to="/blog" class="nav-navigation--slide-link">
+              Articles
+            </nuxt-link>
+          </li>
+          <li class="nav-navigation--slide-item">
+            <nuxt-link to="/portfolio" class="nav-navigation--slide-link">
+              Portfolio
+            </nuxt-link>
+          </li>
+        </div>
+      </transition>
+      <ul
+        class="nav-navigation--list"
+        :class="{ 'nav-navigation--list__visible': menuIsVisible }"
+      >
         <li class="nav-navigation--list-item">
           <nuxt-link to="/" class="nav-navigation--list-link">
             Home
@@ -26,8 +57,17 @@
 </template>
 
 <script>
-
 export default {
-  name: 'AppNav'
+  name: 'AppNav',
+  data () {
+    return {
+      menuIsVisible: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.menuIsVisible = !this.menuIsVisible
+    }
+  }
 }
 </script>
